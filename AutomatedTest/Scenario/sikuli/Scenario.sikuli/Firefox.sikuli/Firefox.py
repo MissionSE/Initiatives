b77d8afc5b0576b08f4ebb9e58d4925fef1afe8d
autotest <autotest@RHEL1.UIRD.LOCAL>
2014-02-27 13:54:55 -0500

    add completed sikuli scenario and docs

--- AutomatedTest/Scenario/sikuli/Scenario.sikuli/Firefox.sikuli/Firefox.py
+++ AutomatedTest/Scenario/sikuli/Scenario.sikuli/Firefox.sikuli/Firefox.py
@@ -1,15 +1,15 @@
 from sikuli import *
 
 def start():
-    click("1393439837193.png")
+    click(Pattern("FirefoxLaunchIcon.png").similar(0.80))
 
 def navigate(url):
-    click(Pattern("1393449125404.png").similar(0.80))
-    
+    doubleClick(Pattern("FirefoxAddressBar.png").similar(0.80).targetOffset(-49,2))
+    type(url + "\n")
 
 def quit():
-    click(Pattern("1393441545842.png").similar(0.95).targetOffset(-1,10))
-    click(Pattern("1393441906918.png").targetOffset(-13,31))
+    click(Pattern("FirefoxFileMenu.png").similar(0.95).targetOffset(-1,10))
+    click(Pattern("FirefoxQuitFromMenu.png").targetOffset(-13,31))
     
     
     
