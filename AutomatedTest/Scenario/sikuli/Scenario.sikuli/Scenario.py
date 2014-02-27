be4a74336c389ad4ed834f826797d1a893b5606b
autotest <autotest@RHEL1.UIRD.LOCAL>
2014-02-27 13:54:55 -0500

    add completed sikuli scenario and docs

--- AutomatedTest/Scenario/sikuli/Scenario.sikuli/Scenario.py
+++ AutomatedTest/Scenario/sikuli/Scenario.sikuli/Scenario.py
@@ -1,10 +1,67 @@
 sys.path.append("/home/autotest/AutomatedTest/Scenario/sikuli/Scenario.sikuli")
 
 import Firefox
+import Ciwi
 reload(Firefox)
+reload(Ciwi)
 
-#Firefox.start()
+def testCiwiInstall():
+    
+    Ciwi.startInstall()
+
+    Ciwi.enterRemoteLoginCredentials()
+
+    Ciwi.enterInstallPath("/home/autotest/ciwi")
+
+    Ciwi.enterSiteSpecificQuestions()
+
+    Ciwi.enterPhysicalHosts()
+
+    Ciwi.submitTransaction()
+
+    Ciwi.gotoAdsComponentStatusByHost()
+    
+    wait("CiwiRhel1Installed.png", 20)
+
+
+def testCiwiUninstall():
+    
+    Ciwi.startUninstall()
+
+    Ciwi.enterRemoteLoginCredentials()
+
+    Ciwi.enterSiteSpecificQuestions()
+
+    Ciwi.enterPhysicalHosts()
+
+    Ciwi.submitTransaction()
+
+    Ciwi.gotoAdsComponentStatusByHost()    
+    
+    wait(Pattern("CiwiRhel1Uninstalled.png").exact(), 20)
+
+
+# Start of main routine
+Firefox.start()
 
 Firefox.navigate("rhelserv1/ciwi/ciwi")
 
-#Firefox.quit()
+Ciwi.doLogin()
+
+Ciwi.doSelectDomain()
+
+Ciwi.doMainMenuComponents()
+
+Ciwi.gotoAdsComponentStatusByHost()
+
+testCiwiInstall()
+
+testCiwiUninstall()
+  
+Ciwi.doLogout()
+
+Firefox.quit()
+
+
+
+  
