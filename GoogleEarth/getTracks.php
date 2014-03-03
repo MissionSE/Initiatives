aa13f15f4030e122e8dcf83550fc2b3d31fd60c0
williamganley <william.ganley@missionse.com>
2014-03-03 17:58:40 -0500

    style camera tilt widget dynamically update close control

--- GoogleEarth/getTracks.php
+++ GoogleEarth/getTracks.php
@@ -8,7 +8,7 @@ $con = mysql_connect("localhost", "test", "test") or die('Sorry, could not conne
 
 mysql_select_db("earth", $con) or die('Sorry, could not connect to database');
     
-$query = "SELECT trackNum,trackCategory,trackIdentity,trackPlatform,trackLat,trackLon,trackAlt from tracks";
+$query = "SELECT trackNum,trackCategory,trackIdentity,trackPlatform,trackLat,trackLon,trackAlt,trackCourse,trackSpeed from tracks";
 
 $result = mysql_query($query) or die('Sorry, could not find any tracks');
 
