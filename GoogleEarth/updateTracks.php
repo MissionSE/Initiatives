bbb708f27825d03bd37ca8603017e0ac1f666f90
williamganley <william.ganley@missionse.com>
2014-03-05 13:43:58 -0500

    Add dynamic track driver, view offset locations, additional gui styles

--- GoogleEarth/updateTracks.php
+++ GoogleEarth/updateTracks.php
@@ -6,7 +6,7 @@ $con = mysql_connect("localhost", "test", "test") or die('Sorry, could not conne
 
 mysql_select_db("earth", $con) or die('Sorry, could not connect to database');
     
-$selectQuery = "SELECT trackNum,trackLat,trackLon,trackAlt from tracks";
+$selectQuery = "SELECT trackNum,trackLat,trackLon,trackAlt,trackCourse,trackSpeed from tracks";
 
 $selectResult = mysql_query($selectQuery) or die('Sorry, could not find any tracks');
 
@@ -22,19 +22,38 @@ while($row = mysql_fetch_array($selectResult, MYSQL_ASSOC)) {
 
     $trackAlt = $row['trackAlt'];
     
-    if ("true" == $incrementVar) {
-        //Increment the lat and lon of each track
-        $trackLat += $delta;
-    }
-    else {
-        $trackLat -= $delta;
-    }
+    $trackCourse = $row['trackCourse'];
     
-    $updateQuery =  "UPDATE tracks SET trackLat='$trackLat', trackLon = '$trackLon', " .
-                    "trackAlt = $trackAlt WHERE trackNum = $trackNum";
+    $trackSpeed = $row['trackSpeed'];
+    
+    //Flip the track course into opposite direction
+    //if ("true" == $incrementVar) {
+    //    if ($trackCourse < 180) {
+    //        $trackCourse = $trackCourse + 180;    
+    //    }
+    //    else {
+    //        $trackCourse = $trackCourse - 180;
+    //    }
+    //}
+    
+    //Determine R in meters based on speed
+    //R = xxx mi/hr x 1609.333 m/mi x 1/1800 hr/2sec
+    $R = $trackSpeed * 1609.333 * (1/1800);
+    
+    //Calculate new position based on course and speed
+    $dx = $R*sin(deg2rad($trackCourse));
+    $dy = $R*cos(deg2rad($trackCourse));
+
+    $deltaLon = $dx/(111320*cos(deg2rad($trackLat)));
+    $deltaLat = $dy/110540;    
+
+    $newLon = $trackLon + $deltaLon;
+
+    $newLat = $trackLat + $deltaLat;
+    
+    $updateQuery =  "UPDATE tracks SET trackLat='$newLat', trackLon = '$newLon', " .
+                    "trackAlt = '$trackAlt', trackCourse = '$trackCourse' WHERE trackNum = $trackNum";
 
     $updateResult = mysql_query($updateQuery) or die(mysql_error());
-    
-    //echo "trackNum:$trackNum trackLat:$trackLat trackLon:$trackLon increment:$incrementVar<br>\n";
 }
 ?>
